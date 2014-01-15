a1425e1cff1e89b00b9f1d40e330f4a99ae034cd
mclawges22 <mclawges22@gmail.com>
2014-01-15 13:29:10 -0500

    practice printing

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+#learning python commands
+#by Maegan Clawges
+
+print "Hello world!"
+
+print "My name is Maegan"
+
+print "let's count some things:"
+
+print "Boys", 25+ 30 / 6
+
+print "Girls", 100 - 25 * 3 % 4
+print "Let's count the class:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
