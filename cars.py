ba64ee26f742f80542c7f8bff459ed002d1f4fcf
mclawges22 <mclawges22@gmail.com>
2014-01-15 13:54:51 -0500

    second way of declaring variables

--- cars.py
+++ cars.py
@@ -11,5 +11,9 @@ average_passengers_per_car = passengers / cars_driven
 
 print "There are", cars, "cars available."
 print "There are only", drivers, "drivers available."
-print "We can transport", carpool_capacity, "people today."
-print "we need to put", average_passengers_per_car, "in each car."
+#print "We can transport", carpool_capacity, "people today."
+print "We can transport %d people today." % carpool_capacity
+#print "We need to put", average_passengers_per_car, "in each car."
+print "We need to put %d in each car." % average_passengers_per_car
+
+print "We need to put %d in each of the %d cars." % (average_passengers_per_car, cars_driven)
